97c807fd7a0b7501fea53420191fb4458b9ed9e9
aitagueye-byyte <TON_EMAIL_GITHUB>
2026-09-12 20:04:12 +0000

    fix: safeLoad pour dotenv et nettoyage des variables entrypoint

--- database/migrate.php
+++ database/migrate.php
@@ -7,7 +7,8 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 use Dotenv\Dotenv;
 
 $dotenv = Dotenv::createImmutable(dirname(__DIR__));
-$dotenv->load();
+// $dotenv->load();
+$dotenv->safeLoad();
 
 $capsule = require dirname(__DIR__) . '/config/database.php';
 
