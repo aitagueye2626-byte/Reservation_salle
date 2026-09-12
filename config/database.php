a42812fdbc38e82287163b673b2d87e090e9b292
aitagueye-byyte <TON_EMAIL_GITHUB>
2026-09-12 09:09:00 +0000

    feat: supporter les connexions MySQL avec SSL (compatibilité TiDB Cloud/Render)

--- config/database.php
+++ config/database.php
@@ -8,6 +8,12 @@ $dotenv->load();
 
 $capsule = new Capsule();
 
+$options = [];
+
+if (($_ENV['DB_SSL'] ?? 'false') === 'true') {
+    $options[\PDO::MYSQL_ATTR_SSL_CA] = $_ENV['DB_SSL_CA'] ?? '/etc/ssl/certs/ca-certificates.crt';
+}
+
 $capsule->addConnection([
     'driver'    => $_ENV['DB_DRIVER'],
     'host'      => $_ENV['DB_HOST'],
@@ -18,6 +24,7 @@ $capsule->addConnection([
     'charset'   => 'utf8mb4',
     'collation' => 'utf8mb4_unicode_ci',
     'prefix'    => '',
+    'options'   => $options,
 ]);
 
 $capsule->setAsGlobal();
